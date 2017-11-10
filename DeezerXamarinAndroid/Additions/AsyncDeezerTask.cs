94fd7b34fbf2d22725935500657bdf37a5432328
CodeMyst <codemyst@outlook.com>
2017-11-10 20:04:44 +0100

    Added aar and metadata. Everything should work now. - Still gonna do some testings and maybe add helpers to easily get information and do stuff.

---
+++ DeezerXamarinAndroid/Additions/AsyncDeezerTask.cs
@@ -0,0 +1,55 @@
+﻿using Android.OS;
+using Deezer.Connect;
+using Deezer.Network.Request.Event;
+using Java.Lang;
+
+namespace Deezer.Network.Request
+{
+    public class AsyncDeezerTask : AsyncTask<DeezerRequest, Void, string>
+    {
+        private DeezerConnect deezerConnect;
+        private IRequestListener requestListener;
+        private Object requestId;
+        private Exception exception;
+
+        public AsyncDeezerTask (DeezerConnect deezerConnect, IRequestListener requestListener)
+        {
+            this.deezerConnect = deezerConnect;
+            this.requestListener = requestListener;
+        }
+
+        protected override string RunInBackground (params DeezerRequest [] requestList)
+        {
+            if (requestList == null || requestList.Length != 1 || requestList [0] == null)
+            {
+                throw new IllegalArgumentException ("AsyncDeezerTask executes only on request, and doesn't support null requests.");
+            }
+
+            DeezerRequest request = requestList [0];
+            requestId = request.Id;
+            exception = null;
+            try
+            {
+                return deezerConnect.RequestSync (request);
+            }
+            catch (Exception localException)
+            {
+                exception = localException;
+                return localException.ToString ();
+            }
+        }
+
+        protected override void OnPostExecute (string response)
+        {
+            base.OnPostExecute (response);
+
+            if (exception == null)
+            {
+                requestListener.OnComplete (response, requestId);
+                return;
+            }
+
+            requestListener.OnException (exception, requestId);
+        }
+    }
+}
